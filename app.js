123f737148ec1381eb71d5e54939d5e7f5f9799a
wipha-byte <66072902+wipha-byte@users.noreply.github.com>
2020-12-11 01:39:47 +0800

    first profile test

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
+//journal variables
 const submitBtn = document.getElementById('submit-btn');
+const clearBtn = document.getElementById('clear-btn');
 var urlInput = document.querySelector("#url");
 var titleInput = document.querySelector("#entryTitle");
 var captionInput = document.querySelector("#entryCaption");
 var textContentInput = document.querySelector("#content-input");
 var dateInput = document.querySelector("#dateInput");
+var cardContent = document.querySelector("#card-content");
+
+
+
+//function Entry(title, caption, text, date, url){
+//  this.title = titleInput.value();
+//  this.caption = captionInput.value();
+//  this.text = textContentInput.value();
+//  this.date = dateInput.value();
+//  this.url = urlInput.value();
+//}
 
 submitBtn.addEventListener("click", function(){
-    const url = urlInput.value;
-    const title = titleInput.value;
-    const caption = captionInput.value;
-    const date = dateInput.value;
 
+  var url = urlInput.value;
+  var title = titleInput.value;
+  var caption = captionInput.value;
+  var textContent = textContentInput.value;
+  var date = dateInput.value;
+  
     $("#card-content").append(
         `<ion-card>
         <img src="${url}" />
@@ -20,9 +35,43 @@ submitBtn.addEventListener("click", function(){
         </ion-card-header>
         <ion-card-content>
          <b>${caption}</b>
-         <p></p>
+         <p>${textContent}</p>
         </ion-card-content>
       </ion-card>`
     );
+    urlInput.value = "";
+    titleInput.value = "";
+    captionInput.value = "";
+    dateInput.value = "";
+    textContentInput.value = "";
 
+});
+
+
+clearBtn.addEventListener("click", function(){
+  urlInput.value = "";
+  titleInput.value = "";
+  captionInput.value = "";
+  dateInput.value = "";
+  textContentInput.value = "";
+});
+
+//Profile
+var editProfileBtn = document.querySelector("#edit-profile");
+var createProfileBtn = document.querySelector("#create-profile");
+var firstNameInput = document.querySelector("#firstName-input");
+var lastNameInput = document.querySelector("#lastName-input");
+var genderInput = document.querySelector("#gender-input");
+var emailInput = document.querySelector("#email-input")
+
+
+
+editProfileBtn.addEventListener("click", function(){
+  var firstName = firstNameInput.value;
+  var lastName = lastNameInput.value;
+  var gender = genderInput.value;
+  var email = emailInput.value;
+
+  document.getElementById("p-name").innerHTML = `${firstName}`;
+  document.getElementById("p-gender").innerHTML = `${gender}`;
 });
